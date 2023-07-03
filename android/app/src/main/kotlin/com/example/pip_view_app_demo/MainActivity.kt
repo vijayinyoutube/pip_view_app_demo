ac925f4dd079b0638b89a70f11e25291e53ee4be
vijayinyoutube <vijaythevirtue@gmail.com>
2023-07-03 22:08:08 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/pip_view_app_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pip_view_app_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/background.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BackGroundScreen extends StatelessWidget {
+  const BackGroundScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('It\'s the background screen')),
+    );
+  }
+}
